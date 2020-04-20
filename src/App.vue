fc09d71234d37c10333c87e26f72e51edd8b9cf6
Maxim Slastyon <maximslastyon@Maxims-MacBook-Pro.local>
2020-04-20 23:10:03 +0300

    add route

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <!-- <img src="./assets/logo.png" /> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view></router-view>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
